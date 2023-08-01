8998ce5fce68eb1fd2199f0093b5c9a19719dcf9
Ahmed Goda <ahmedgodaa2001@gmail.com>
2023-08-01 08:49:07 +0300

    Update NumberOfOrdersMadeByEachCustomer.sql

--- scripts/NumberOfOrdersMadeByEachCustomer.sql
+++ scripts/NumberOfOrdersMadeByEachCustomer.sql
@@ -1,5 +1,5 @@
 SELECT Customers.customer_name, COUNT(Orders.order_id) AS num_orders
 FROM Customers
 LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
-GROUP BY Customers.customer_id
+GROUP BY Customers.customer_id;
 
